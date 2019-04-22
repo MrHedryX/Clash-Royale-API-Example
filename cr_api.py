aaa19e66b5ba49b4227a6e26e3d7e51ffabe4cc9
MrHedryX <Mr.HedryX@gmail.com>
2019-04-22 17:55:47 +0200

    Update cr_api.py

--- cr_api.py
+++ cr_api.py
@@ -5,13 +5,13 @@ apiURL = 'https://api.clashroyale.com/v1'
 
 #Create the API request
 request = urllib.request.Request(
-    apiURL + '/cards',
+    apiURL + '/cards', #In this example, we will request to view all Clash Royale cards available
     headers={'Authorization': 'Bearer %s' % key}
 )
 
 #Send the API request
 response = urllib.request.urlopen(request).read()
 
-#Load and print the API response
+#Load and pretty print the API response
 data = json.loads(response)
 print(json.dumps(data, indent=4))
